abdc469a5eaba65085f510b34befe605f3af5944
Patrick Kerwood <patrick@kerwood.dk>
2019-08-12 19:13:20 +0200

    Fixes..

--- assets/init.sh
+++ assets/init.sh
@@ -6,11 +6,23 @@ function echo_log {
     echo "$TIMESTAMP $*"
 }
 
-# IF UID/GID BLA
+if [ -z ${UID+x} ]; then
+    OLD_UID=$(id -u rtorrent)
+    usermod -u $UID rtorrent
+    find / -user $OLD_UID -exec chown -h rtorrent {} \;
+fi
+
+if [ -z ${GID+x} ]; then
+    OLD_GID=$(id -G rtorrent)
+    usermod -G $GID rtorrent
+    find / -group $OLD_GID -exec chgrp -h rtorrent {} \;
+fi
 
 echo_log "--> INFO Starting rtorrent..."
 supervisorctl start rtorrent
 
-# IF
-echo_log "--> INFO Starting lighttpd.."
-supervisorctl start lighttpd
+# ENABLE_SCGI
+if [ $ENABLE_SCGI = "true" ]; then
+    echo_log "--> INFO Starting lighttpd.."
+    supervisorctl start lighttpd
+fi
